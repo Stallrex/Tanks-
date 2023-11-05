655accedffdb81bcf3e7b140174e6fcd9c8ad9ce
Stallrex <140261252+Stallrex@users.noreply.github.com>
2023-11-05 17:09:33 +0200

    Added all the stuff

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
 
+    public GameObject explosion;
+    public float maxDamage = 4;
+    public float minDamage = 1;
+
     public float speed = 20;
     public float lifeTime = 5;
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, lifeTime);
+        Invoke(nameof(Explode), lifeTime);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += transform.forward * speed * Time.deltaTime;
+    }
+    private void OnCollisionEnter(Collision other) {
+        Health health = other.gameObject.GetComponent<Health>();
+        if(health != null){
+
+            var damage = Random.Range(minDamage, maxDamage + 1);
+            health.TakeDamage(damage);
+
+        }
+        Explode();
+    }
+
+    void Explode(){
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+
     }
 }
